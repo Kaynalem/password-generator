9170d2297c81249cbef7bed449d70b0d480e211d
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-04-05 12:39:19 -0500

    clear debugging console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,8 @@ function generatePassword() {
 
   //check input meets requirements (number between 8 and 128)
   while ((passwordLength < 8 || passwordLength > 128) || isNaN(passwordLength)) {
-    console.log("made it into the while ((passwordLength < 8 || passwordLength > 128) || isNaN(passwordLength)) statement, now trying confirm");
+    //window.alert here was being used, but was not showing in Chrome on live site. Using confirm for compatibility.
     window.confirm("You need to provide a valid answer! Please enter a number between 8 and 128.");
-    console.log("number confirm should have fired");
     passwordLength = window.prompt("How many characters would you like your password to be? Please enter a number between 8 and 128.");
     passwordLength = parseInt(passwordLength);
   }
@@ -53,9 +52,8 @@ function generatePassword() {
     console.log("The value of passwordSelection is: " + passwordSelection);
     // if at least one of the above character sents is not selected restart questions at passwordLower
     if (!passwordSelection) {
-      console.log("made it into the !passwordSelection if statement, now trying confirm");
+      //window.alert here was being used, but was not showing in Chrome on live site. Using confirm for compatibility.
       window.confirm("At least one character option must be selected.");
-      console.log("passwordSelection confirm should have fired");
     }
   }
   // generates random password based on selections
