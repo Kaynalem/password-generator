dfa6c590a93561a768a6eae29b7bf894c7d9f541
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-04-05 15:24:38 -0500

    add function and logic to verify user selected requirements are met

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,51 +20,130 @@ function generatePassword() {
   var passwordSelection = false;
   //this will be the set of characters used in the generated password
   var characterSet = '';
+  //instantiate user selection variables
+  var passwordLower = "";
+  var passwordUpper = "";
+  var passwordNumeric = "";
+  var passwordSpecial = "";
+  //instantiate character sets
+  var lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+  var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var specials = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~\"'"; //OWASP approved set ignoring space
+
+
   while (!passwordSelection) {
     // ask the user if they want lowercase characters
-    var passwordLower = window.confirm("Do you want lowercase characters?");
+    passwordLower = window.confirm("Do you want lowercase characters?");
     if (passwordLower) {
-      characterSet += 'abcdefghijklmnopqrstuvwxyz';
+      characterSet += lowerLetters;
       passwordSelection = true;
     }
 
     // ask the user if they want uppercase characters
-    var passwordUpper = window.confirm("Do you want uppercase characters?");
+    passwordUpper = window.confirm("Do you want uppercase characters?");
     if (passwordUpper) {
-      characterSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      characterSet += upperLetters;
       passwordSelection = true;
     }
 
     // ask the user if they want numeric characters
-    var passwordNumeric = window.confirm("Do you want numbers?");
+    passwordNumeric = window.confirm("Do you want numbers?");
     if (passwordNumeric) {
-      characterSet += '0123456789';
+      characterSet += numbers;
       passwordSelection = true;
     }
 
     // ask the user if they want special characters
-    var passwordSpecial = window.confirm("Do you want special characters?");
+    passwordSpecial = window.confirm("Do you want special characters?");
     if (passwordSpecial) {
-      characterSet += "!#$%&()*+,-./:;<=>?@[\]^_`{|}~\"'"; //OWASP approved set ignoring space
+      characterSet += specials; 
       passwordSelection = true;
     }
     
     
-    // if at least one of the above character sents is not selected restart questions at passwordLower
+    // if at least one of the above character sets is not selected restart questions at passwordLower
     if (!passwordSelection) {
       //window.alert here was being used, but was not showing in Chrome on live site. Using confirm for compatibility.
       window.confirm("At least one character option must be selected.");
     }
   }
-  // generates random password based on selections
-  var passwd = '';
 
-  for(var i = 0; i < passwordLength; i++) {
-    var positionRandom = Math.floor(Math.random() * characterSet.length); 
+  function randomPassword(){
+     // generates random password based on selections
+    var passwd = '';
+    for(var i = 0; i < passwordLength; i++) {
+      var positionRandom = Math.floor(Math.random() * characterSet.length); 
 
-    passwd += characterSet.charAt(positionRandom);
+      passwd += characterSet.charAt(positionRandom);
+    }
+    return passwd;
   }
-  return passwd;
+
+  //checks to see if provided password (passwd) contains at least one character from the user selected character sets
+  function charsMatch(passwd, charSet) {
+    var result = false;
+    for(var i = 0; i < charSet.length; i++) {
+      // if password includes a given character in the provided character set, stop checking and return true
+      if(passwd.includes(charSet.charAt(i))) {
+        result = true;
+        break;
+      }
+    }
+    return result;
+  }
+  
+  var completedPassword = "";
+  var ready = false;
+  var ready2 = false;
+  // continuously runs randomPassword to generate a password (ready) then checks each user selected character set to see if the password contains a character from that set (ready2)
+  while(!ready) {
+    completedPassword = randomPassword();
+    while(!ready2) {
+    if (passwordLower) { //if user selected lowercase, run this check
+      if(charsMatch(completedPassword, lowerLetters)) {//check if contains lower
+        ready = true;
+        ready2= true;
+      } else { // if no lower, break loop and generate new password and check again
+        ready = false;
+        ready2= false;
+        break;
+      }
+    }
+    if (passwordUpper) { //if user selected uppercase, run this check
+      if(charsMatch(completedPassword, upperLetters)) {//check if contains upper
+        ready = true;
+        ready2= true;
+      } else { // if no upper, break loop and generate new password and check again
+        ready = false;
+        ready2= false;
+        break;
+      }
+    }
+    if (passwordNumeric) { //if user selected numbers, run this check
+      if(charsMatch(completedPassword, numbers)) {//check if contains number
+        ready = true;
+        ready2= true;
+      } else {
+        ready = false;
+        ready2= false;
+        break;
+      }
+    }
+    if (passwordSpecial) { //if user selected special characters run this check
+      if(charsMatch(completedPassword, specials)) {//check if contains special characgters
+        ready = true;
+        ready2= true;
+      } else { // if no special characters , break loop and generate new password and check again
+        ready = false;
+        ready2= false;
+        break;
+      }
+    }
+  }
+}
+
+return completedPassword;
 
 }
 // Get references to the #generate element
